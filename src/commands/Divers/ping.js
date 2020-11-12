7e732df6fde2c83d713141d3f69d31aacd6449c8
Remi Launay <rlaunay25@gmail.com>
2020-11-12 14:50:33 +0100

    :bug: 312eme reglage du calendrier '-'

--- src/commands/Divers/ping.js
+++ src/commands/Divers/ping.js
@@ -1,18 +1,18 @@
 module.exports.run = (client, message, args) => {
-    const start = Date.now();
-    message.channel.send('Pong').then((res) => {
-        res.edit(`Pong : **${Date.now() - start} ms**`);
-    });
-}
+	const start = Date.now();
+	message.channel.send('Pong').then((res) => {
+		res.edit(`Pong : **${Date.now() - start} ms**`);
+	});
+};
 
 module.exports.help = {
-    name: "ping",
-    aliases: ['ping'],
-    category: 'divers',
-    description: "Renvoie pong!",
-    usage: '',
-    cooldown: 10,
-    admin: false,
-    permissions: [],
-    args: false
-}
+	name: 'ping',
+	aliases: ['ping'],
+	category: 'divers',
+	description: 'Renvoie pong!',
+	usage: '',
+	cooldown: 10,
+	admin: false,
+	permissions: [],
+	args: false,
+};
